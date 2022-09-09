35180e244062e920efa9318090dab5700f2a5881
KeithKoogen <keithkmoodley@hotmail.com>
2022-09-08 23:50:45 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lower case
+ * Returns: returns 0
+ */
+
+int main(void)
+{
+
+for (int i=0; i < 26 ; i++)
+{
+putchar(i+97);
+}
+
+putchar('\n');
+
+return (0);
+
+}
