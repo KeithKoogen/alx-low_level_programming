78f4fa58ff843e3475cb9652f509a9092ea8e717
KeithKoogen <keithkmoodley@hotmail.com>
2022-09-10 14:10:29 -0700

    102s

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,51 +1,89 @@
 #include <stdio.h>
 /**
-* main - print 2 digit number combinations not repeating
-* Return: 0
-*/
+ * main - print 2 digit number combinations not repeating
+ * Return: 0
+ */
 
 int main(void)
 {
-int i;
-int j;
-int k;
-int m;
+  int i;
+  int j;
+  int k;
+  int m;
 
-for (i = 0; i < 10; i++)
-{
+  for (i = 0; i < 10; i++)
+    {
+     
 
 
-for (j = 0; j < 10; j++)
-{
+      for (j = 0; j < 10; j++)
+	{
+	  m = j + 1;
 
-for (k = i; k < 10; k++)
-{
-for (m = j + 1; m < 10; m++)
-{
+	  for (k = i; k < 10; k++)
+	    {
+	        
+	      
+	      do
+		{
 
-putchar(i + 48);
-putchar(j + 48);
-putchar(32);
-putchar(k + 48);
-putchar(m + 48);
+		  if (i == 9 && j == 9 && k == 9 && m == 10)
+		    
 
-if (i == 9 && j == 8 && k == 9 && m == 9)
-{
+		    
+		    {
+		      
 
-}
-else
-{
-putchar(44);
-putchar(32);
-}
+		      
+		      break;
+		      
 
-}
-}
-}
+		      
 
-}
+		      
+		    }        
 
-putchar('\n');
-return (0);
+		  
+		putchar(i + 48);
+		putchar(j + 48);
+		putchar(32);
+		putchar(k + 48);
+		putchar(m + 48);
+		
+		if (i == 9 && j == 8 && k == 9 && m == 9)
+		  
+		  {
+		    
+		    break;
+		    
+		    
+		  } 
+		
+    
+		putchar(44);
+		putchar(32);
+      
+
+      
+
+		  
+		m++;
+	      }
+	      while(m < 10);
+	         
+	      m = 0;
+	      
+	      
+
+
+
+
+	    }
+	}
+
+    }
+
+  putchar('\n');
+  return (0);
 
 }
