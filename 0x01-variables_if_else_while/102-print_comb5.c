30987b7961d10ee8352b970dee1e3606d7a8687b
KeithKoogen <keithkmoodley@hotmail.com>
2022-09-10 14:30:18 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,84 +6,41 @@
 
 int main(void)
 {
-  int i;
-  int j;
-  int k;
-  int m;
+int i;
+int j;
+int k;
+int m;
 
-  for (i = 0; i < 10; i++)
-    {
-     
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+m = j + 1;
+for (k = i; k < 10; k++)
+{
+for (; m < 10 ; m++)
+{
 
-
-      for (j = 0; j < 10; j++)
-	{
-	  m = j + 1;
-
-	  for (k = i; k < 10; k++)
-	    {
-	        
-	      
-	      for (; m < 10 ; m++)
-		{
-
-		  if (i == 9 && j == 9 && k == 9 && m == 10)
-		    
-
-		    
-		    {
-		      
-
-		      
-		      break;
-		      
-
-		      
-
-		      
-		    }        
-
-		  
-		putchar(i + 48);
-		putchar(j + 48);
-		putchar(32);
-		putchar(k + 48);
-		putchar(m + 48);
-		
-		if (i == 9 && j == 8 && k == 9 && m == 9)
-		  
-		  {
-		    
-		    break;
-		    
-		    
-		  } 
-		
-    
-		putchar(44);
-		putchar(32);
-      
-
-      
-
-		  
-		
-	      }
-	      
-	         
-	      m = 0;
-	      
-	      
-
-
-
-
-	    }
-	}
-
-    }
-
-  putchar('\n');
-  return (0);
-
+if (i == 9 && j == 9 && k == 9 && m == 10)
+{
+break;
+}
+putchar(i + 48);
+putchar(j + 48);
+putchar(32);
+putchar(k + 48);
+putchar(m + 48);
+if (i == 9 && j == 8 && k == 9 && m == 9)
+{
+break;
+}
+putchar(44);
+putchar(32);
+}
+m = 0;
+}
+}
+}
+putchar('\n');
+return (0);
 }
