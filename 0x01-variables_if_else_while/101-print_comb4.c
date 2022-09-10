0af95a3fe1570bad98da05d434311ed2fc48f2cd
KeithKoogen <keithkmoodley@hotmail.com>
2022-09-10 11:01:50 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+* main - print 2 digit number combinations not repeating
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+int j;
+int k;
+
+for (i = 0; i < 10; i++)
+{
+  for (j = i + 1; j < 10; j++)
+    {
+
+
+      for (k = j + 1; k < 10; k++)
+	{
+
+	  putchar(i + 48);
+	  putchar(j + 48);
+	  putchar(k + 48);
+	  if (i < 7)
+	    {
+	      putchar(44);
+	      putchar(32);
+	    }
+	  }
+     }
+}
+
+putchar('\n');
+return (0);
+
+}
