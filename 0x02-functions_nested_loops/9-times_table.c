8b70d0002ba7bf17b4db0c4b0e7c730f646d51c7
KeithKoogen <keithkmoodley@hotmail.com>
2022-09-13 12:00:21 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,32 +8,38 @@
 void times_table(void)
 {
 
-  int i;
-  int j;
-  int firstnum;
-  int lastnum;
+int i;
+int j;
+int firstnum;
+int lastnum;
 
-  for (i = 0; i < 10; i++)
-    {
-      for (j = 0; j < 10; j++)
-	{
-	  if ((i * j) >= 10)
-	    {
-	      firstnum = (i * j) / 10;
-	      lastnum = (i * j) % 10;
-	      _putchar(firstnum + 48);
-	      _putchar(lastnum + 48);
-	    } else _putchar((i * j) + 48);
-	  if (j < 9)
-	    {
-	  _putchar(44);
-	  _putchar(32);
-	  _putchar(32);
-	    }
-	    }
-      _putchar('\n');
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if ((i * j) >= 10)
+{
+firstnum = (i * j) / 10;
+lastnum = (i * j) % 10;
+_putchar(firstnum + 48);
+_putchar(lastnum + 48);
+} else _putchar((i * j) + 48);
+if (j < 9)
+{
+_putchar(44);
+if (j * i >= 10)
+{
+_putchar(32);
+}
+else
+{
+_putchar(32);
+_putchar(32);
+}
+}
+}
+_putchar('\n');
+}
 
-    }
-  
 
 }
