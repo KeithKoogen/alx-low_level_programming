807f6a890e05590798c95d093c683622dfc1e3c3
KeithKoogen <keithkmoodley@hotmail.com>
2022-09-13 12:14:56 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * add - add two integers and returns result
+ * Return: Integer
+ */
+
+int add(int a, int b)
+{
+  return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -23,3 +23,4 @@ int _abs(int);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
+int add(int a, int b);
