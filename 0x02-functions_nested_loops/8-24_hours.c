99947d9c839f149e5fafcb59845df2b1ea130b34
KeithKoogen <keithkmoodley@hotmail.com>
2022-09-13 11:14:25 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+int i;
+int j;
+int k;
+int m;
+
+for (i = 0; i < 3; i++)
+{
+  
+if ((i == 2) && (j == 3) && (k == 5) && (m == 9))
+{
+ break;
+}
+ 
+for (j = 0; j < 10; j++)
+{
+ 
+if ((i == 2) && (j == 3) && (k == 5) && (m == 9))
+{
+break;
+}
+	      
+for (k = 0; k < 6; k++)
+{
+if ((i == 2) && (j == 3) && (k == 5) && (m == 9))
+{
+    break;
+}
+for (m = 0; m < 10; m++)
+{
+_putchar(i + 48);
+_putchar(j + 48);
+_putchar(':');
+_putchar(k + 48);
+_putchar(m + 48);
+_putchar('\n');
+if ((i == 2) && (j == 3) && (k == 5) && (m == 9))
+{
+break;
+}
+}
+}
+   
+}
+ if ((i == 2) && (j == 3) && (k == 5) && (m == 9))
+   break;
+}
+  
+}
