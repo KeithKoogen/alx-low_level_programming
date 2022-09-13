bed0712d07fec68be9521aadf43d2e58f260158f
KeithKoogen <keithkmoodley@hotmail.com>
2022-09-13 01:30:06 -0700

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+int i;
+
+for (i = 0; i < 26; i++)
+{
+_putchar(i + 97);
+}
+
+_putchar('\n');
+
+}
