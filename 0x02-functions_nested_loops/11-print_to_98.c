a325f524ea8789d2c0ee5a1c87f394b26ca6fe51
KeithKoogen <keithkmoodley@hotmail.com>
2022-09-13 12:48:06 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,26 +9,23 @@
 void print_to_98(int n)
 {
 int i;
-int firstnum;
-int secondnum;
 
+if (n > 98)
+{
+for (i = n; i > 97; i--)
+printf("%d, ",i);
+}
+else
+{
 
- if (n > 98)
-   {
-     for (i = n; i > 97; i--)
-       printf("%d, ",i);
-
-   } else
-   {
- 
-for (i = n; i < 99; i++)
+for (i = n; i < 98; i++)
 {
 
 
 printf("%d, ",i);
- 
+
 }
-   }
- printf("98");
+}
+printf("98");
 _putchar('\n');
 }
