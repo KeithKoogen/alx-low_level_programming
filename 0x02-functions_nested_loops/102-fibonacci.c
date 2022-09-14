bfdd4ec3f3e6e2238c25fe1227b12fe2407ec5b3
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-14 13:36:58 +0200

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+
+/** main - print fibonacci sequence startig with 1 and 2
+* Return: Void
+*/
+
+int main(void)
+{
+int i;
+int j;
+int k;
+int tmp;
+  
+j = 1;
+k = 2;
+printf("%d, ", i);
+printf("%d, ", k);
+  
+for (i = 0; i < 50; i++)
+{
+tmp = k + j;
+j = k;
+k = tmp;
+printf("%d", k);
+if (i == 49) 
+{
+break
+}
+printf(, );
+  
+  
+}
