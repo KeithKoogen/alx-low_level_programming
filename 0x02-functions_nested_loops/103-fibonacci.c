3902825d7b32f917f753e45c9dcf8cd01e73711d
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-14 13:57:38 +0200

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+
+/**
+* main - print fibonacci sequence startig with 1 and 2
+* Return: Void
+*/
+
+int main(void)
+{
+long int i;
+long int j;
+long int k;
+long int tmp;
+long int sum;
+
+j = 1;
+k = 2;
+printf("%ld, ", j);
+printf("%ld, ", k);
+
+while (k < 4000000)
+{
+tmp = k + j;
+j = k;
+k = tmp;
+
+if (k % 2 == 0)
+{
+sum = sum + k;
+}
+}
+printf("%ld\n", sum);
+return (0);
+}
 
