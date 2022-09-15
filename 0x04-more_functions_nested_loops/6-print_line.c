bc3e1c5cbb67ea392fb11475ae0d0e12f4432901
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-15 08:15:37 +0200

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_line - draws a line
+* @n: an integer for the size of the line
+* Return: Void
+*/
+
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
