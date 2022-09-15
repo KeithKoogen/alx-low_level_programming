44e695118b2b1deaa334fb8bd713eddfad7ffb68
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-15 16:34:06 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,6 +6,9 @@
 void more_numbers(void)
 {
 int i;
+int j;
+for (j = 0; j < 10; j++)
+{
 for (i = 0; i < 15; i++)
 {
 if (i < 10)
@@ -14,8 +17,9 @@ _putchar(i + 48);
 }
 else
 {
-_putchar(i + 48 - 10);
+_putchar(i + 48 - 9);
 }
 }
 _putchar('\n');
+}
 }
