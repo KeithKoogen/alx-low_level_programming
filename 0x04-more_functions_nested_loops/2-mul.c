76eed888d0b63ce5c87263bf4a68ed3dba95a9ed
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-15 16:10:02 +0200

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
 * mul - multiplies two integers
 * @a: an integer
 * @b: an integer
 * Return: a * b
 */
-
 int mul(int a, int b)
 {
-return (a * b); 
+return (a * b);
 }
