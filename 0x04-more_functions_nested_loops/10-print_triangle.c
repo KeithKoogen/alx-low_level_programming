63aa3fb9ca43b9735d9edf70885acdaa227ea443
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-15 17:40:57 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,7 +9,6 @@ void print_triangle(int size)
 int i;
 int j;
 int k;
-
 if (size > 0)
 {
 for (i = 0; i < size; i++)
@@ -17,21 +16,16 @@ for (i = 0; i < size; i++)
 for (j = 0; j < size - (i + 1); j++)
 {
 printf(" ");
-    }
-    for (k = 0; k < i + 1; k++)
-    {
-      printf("#");
-    }
-    printf("\n");
-    
-  }
 }
-  else
-  {
-   printf("\n"); 
-  }
-  
-  
-  
-  
+for (k = 0; k < i + 1; k++)
+{
+printf("#");
+}
+printf("\n"); 
+}
+}
+else
+{
+printf("\n"); 
+}
 }
