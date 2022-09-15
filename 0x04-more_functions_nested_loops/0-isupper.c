c33ec0a7568d8edf56f0a3c96256b3e34046c46e
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-15 07:46:30 +0200

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isupper - checks for uppercase character
+* @c: a character int value
+* Return: Integer 1 is uppercase else 0
+*/
+
+int _isupper(int c);
+{
+if ( c >=65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
