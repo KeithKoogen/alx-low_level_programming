fccbe09f8fb09ca03d5e7e3d0c8cd7af0a39e1e2
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-15 17:06:20 +0200

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* main - The Fizz buzz test
+* Return: Void
+*/
+
+int main(void)
+{
+int i;
+for (i = 0; i < 100; i++)
+{
+if (((i + 1) % 3 == 0) && ((i + 1) % 3 == 0))
+{
+printf("FizzBuzz ");
+}
+else if ((i + 1) % 3 == 0))
+{
+printf("Fizz ");
+}
+else if ((i + 1) % 5 == 0))
+{
+printf("Buzz ");
+}
+else
+{
+ printf(i + 1);
+}
+}
+}
