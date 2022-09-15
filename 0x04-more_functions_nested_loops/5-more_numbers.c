e7a2bb305e163d33924ad70da3bb229f0a2f3f44
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-15 16:31:13 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
 * more_numbers -  prints 10 times the numbers, from 0 to 14
 * Return: Void
 */
-
 void more_numbers(void)
 {
 int i;
@@ -16,7 +14,7 @@ _putchar(i + 48);
 }
 else
 {
-_putchar(i + 48 - 9);
+_putchar(i + 48 - 10);
 }
 }
 _putchar('\n');
