6e5465648d985ffe952601925a41de9229b1e27d
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-15 17:28:42 +0200

    Create 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+* print_triangle - prints a triangle, followed by a new line
+* @size: Size of triangle in integer
+* Return: VOid
+*/
+void print_triangle(int size)
+{
+int i;
+int j;
+int k;
+
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size - i; j++)
+{
+printf(" ");
+    }
+    for (k = 0; k < i; k++)
+    {
+      printf("#");
+    }
+    printf("\n");
+    
+  }
+}
+  else
+  {
+   printf("\n"); 
+  }
+  
+  
+  
+  
+}
