b8ac8f08f6ce6b0d84bdb80560cc6539b9885767
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-15 16:53:04 +0200

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,6 +10,8 @@ void print_diagonal(int n)
 {
 int i;
 int j;
+if (n > 0)
+{
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < i + 1; j++)
@@ -17,6 +19,11 @@ for (j = 0; j < i + 1; j++)
 _putchar(32);
 }
 _putchar(92);
+_putchar('\n');
+}
+}
+else
+{
 _putchar('\n');
 }
 }
