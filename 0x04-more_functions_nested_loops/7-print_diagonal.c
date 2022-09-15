479a5a8e0a007c7c0b1d41eaca45bc1f45156f5b
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-15 08:27:54 +0200

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_diagonal - draws a diagonal line
+* @i: an integer for the size of line
+* Return: Void
+*/
+
+void print_diagonal(int n)
+{
+int i;
+int j;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i + 1; j++)
+{
+_putchar(32);
+}
+_putchar(92);  
+}
+_putchar('\n');
+}
