a54cbeb31c83b5b72d5219cf060d20f49534a25d
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-19 12:25:24 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,5 +7,45 @@
 
 int _atoi(char *s)
 {
+int i;
+int j;
+int k;
+int integer;
+integer = 0;
+i = 0;
   
+while (s[i] != '\0')
+{
+if (s[i] >= 48 && s[i] <= 57)
+{
+k = 1;
+if (s[i - 1] == 45)
+{
+j = 1;
+}
+else
+{
+j = 0;
+}
+
+while(s[i] >= 48 && s[i] <= 57)
+{
+integer = integer + (s[i] * k);
+i++;
+k *= 10;
+}
+if ( j == 1)
+{
+return (-integer); 
+}
+else
+{
+return (integer);
+}
+
+}
+i++;
+}
+
+return (integer);
 }
