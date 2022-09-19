c8a57a10e80ecc84f8950735faa06af034d1fbca
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-19 14:08:36 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * _atoi - converts numbersin a string
 * @s: a string
@@ -7,61 +6,41 @@
 
 int _atoi(char *s)
 {
-int j;
-int i;
-int k;
-int count;
-int integer;
-int m;
-int x;
+int j, i, k, count, m, x;
+long int integer;
 integer = 0;
 i = 0;
 k = 1;
 m = 1;
 count = 0;
-  
 while (s[i] != '\0')
 {
 if (s[i] > 47 && s[i] <= 58)
 {
-
 for (x = 0; x < i; x++)
 {
   if (s[x] == '-')
   {
     m = m * -1;
    }
- 
 }
-
 j = i;
-  
 while(s[j] > 47 && s[j] < 58)
 {
-
 j++;
 count++;
 k = k * 10;
 }
-
-j = i;
-  
+j = i;  
 while(s[j] > 47 && s[j] < 58)
 {
 k = k / 10;
 integer = integer + (k * (s[j] - 48));
 j++;
-
-
 }
-
-
 return (integer * m); 
-
-
 }
 i++;
 }
-
 return (integer * m);
 }
