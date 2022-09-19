729354cf4d08337d886356f564886b58052d9ce1
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-19 11:19:28 +0200

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* puts_half - prints second half of string
+* @str: a string
+*/
+
+void puts_half(char *str)
+{
+int i;
+int j;
+i = 0;
+
+while (str[i] != '\0')
+{
+i++;
+}
+
+if (i % 2 == 0)
+{
+for (j = 0; j < i / 2; j++)
+_putchar(j + (i / 2));
+}
+else
+{
+for (j = 0; j < (i - 1) / 2; j++)
+_putchar(j + (i / 2));
+}
+}
