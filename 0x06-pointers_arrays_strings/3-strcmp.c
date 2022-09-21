7f6394050082012fec3edb21afe6550bdb449b8b
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-21 15:43:58 +0200

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strcmp - compares 2 strings
+* @s1:
+* @s2:
+* Return: an integer, 0 is strings match, else a non zero value
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+int i;
+int s1_value;
+int s2_value;
+
+s1_value = 0;
+s2_value = 0;
+i = 0;
+while (s1[i] != '\0')
+{
+	s1_value = s1_value + s1[i];
+	i++;
+}
+
+i = 0;
+while (s2[i] != '\0')
+{
+	s2_value = s1_value + s1[i];
+	i++;
+}
+
+return (s1_value - s2_value);
+}
