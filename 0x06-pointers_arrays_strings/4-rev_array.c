b27ce4fc460a555373cdf9426aea24e7dfb92530
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-21 15:49:53 +0200

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* reverse_array - reverses an array of integers
+* @a: an array of integers
+* @n: the number of elements in the array;
+* Return void
+*/
+
+void reverse_array(int *a, int n)
+{
+	
+	
+}
