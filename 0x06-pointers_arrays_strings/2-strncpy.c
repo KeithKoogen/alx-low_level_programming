50e6ce7e0cd6494b073045b1dce18493f7a6c38d
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-21 15:32:39 +0200

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* _strncpy - copies a string with n value if greater than padded with null
+* @dest: the destination string
+* @src: the source string
+* @n: an integer
+* Return: returns a pointer to copy of a string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int length;
+int i;
+length = 0;
+
+while (src[length] != '\0')
+{
+	++length;
+}
+
+for (i = 0; i < n; i++)
+{
+	if (i < length)
+	{
+		dest[length + i] = src[i];
+	}
+	else
+	{
+		dest[length + i] = '\0';
+	}
+}
+	return (dest);
+}
