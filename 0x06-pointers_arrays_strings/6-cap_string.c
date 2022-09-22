51394f42f4eab05dd633ec7e79a4c22e2202fb62
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-22 17:45:17 +0200

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* cap_string - Capitalizes first words of a string
+* @str: a string
+* Return: a pointer to the string
+*/
+
+char *cap_string(char str*)
+{
+	int i;
+	i = 0;
+if (str[0] > 96 && str[0] < 123)
+	str[0] = str[0] - 32;
+	
+while (str[i] != '\0')
+{
+	if ((str[i] > 31 && str[i] < 48) || (str[i] > 57 && str[i] < 65) || (str[i] > 122 && str[i] < 127) || (str[i] == '\n')
+			{
+				if (str[i + 1] > 96 && str[i + 1] < 123)
+				{
+					str[i + 1] = str[i + 1] - 32;
+				}
+			}
+}
+			return (str);
+	
+}
+  
