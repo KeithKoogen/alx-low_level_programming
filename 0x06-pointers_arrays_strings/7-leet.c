4a89b4e9e1c607ce646a28bed4489acf54062052
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-22 23:10:05 +0200

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* leet - converts a string to leet
+* @str: a string
+* Return: return a pointer to the string
+*/
+
+char *leet(char *str)
+{
+	int i;
+	int j;
+char letter[5][3] = {
+	{'A','a','4'},
+	{'E','e','3'},
+	{'O','o','0'},
+	{'T','t','7'},
+	{'L','l','1'}
+}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+		if (str[i] == letter[j][0] || str[i] == letter[j][1])
+		{
+			str[i] = letter[j][2]
+			}
+		}
+	}
+return (str);
+}
