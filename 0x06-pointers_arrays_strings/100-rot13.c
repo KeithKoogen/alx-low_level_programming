0281505e17ca82c0858b1582fff20f632ac3baf0
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-22 23:43:34 +0200

    Create 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* rot13 - converts a string to rot13
+* @str: a string
+* Return: return a pointer to a string
+*/
+
+char *rot13(char *str)
+{
+	int i;
+	int j;
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char cypher[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	
+	while (str[i] != '\0')
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (alphabet[j] == str[i])
+			{
+				str[i] = cypher[j];
+			}
+		}
+	}
+return (str);
+}
