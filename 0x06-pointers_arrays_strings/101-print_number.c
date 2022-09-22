b00a86294fb8adad18653874e08b782943a8bfb0
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-23 01:04:54 +0200

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,33 +8,31 @@
 
 void print_number(int n)
 {
-	unsigned int num;
-	int i;
-	int j;
-	int times;
-	times = 1;
-	j = 1;
-	i = 0;
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -n;
-	}
-	else
-	{
-		num = n;
-	}
-	
-	while (num / j > 10)
-	{
-		j *= 10;
-		times++;
-	}
-	
-	for (i = 0; i < times + 1; i++)
-	{
-	_putchar((num / j) + 48);
-	j = j / 10;
-		}
+unsigned int num;
+int i;
+int j;
+int times;
+times = 1;
+j = 1;
+i = 0;
+if (n < 0)
+{
+_putchar('-');
+num = -n;
 }
-	
+else
+{
+num = n;
+}
+
+while (num / j > 10)
+{
+j *= 10;
+times++;
+}
+for (i = 0; i < times; i++)
+{
+_putchar((num / j) + 48);
+j = j / 10;
+}
+}
