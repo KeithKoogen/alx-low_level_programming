0eae6aa0306e1ae99c21d7b4130db61f64128054
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-22 18:01:50 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,23 @@
 
 char *cap_string(char *str)
 {
-	int i;
-	i = 0;
+int i;
+i = 0;
 if (str[0] > 96 && str[0] < 123)
-	str[0] = str[0] - 32;
-	
+{
+str[0] = str[0] - 32;
+}
+
 while (str[i] != '\0')
 {
-	if ((str[i] > 31 && str[i] < 48) || (str[i] > 57 && str[i] < 65) || (str[i] > 122 && str[i] < 127) || (str[i] == '\n') || (str[i] == '\t'))
-			{
-				if ((str[i + 1] > 96) && (str[i + 1] < 123))
-				{
-					str[i + 1] = str[i + 1] - 32;
-				}
-			}
-	++i;
+if ((str[i] > 31 && str[i] < 48) || (str[i] > 57 && str[i] < 65) || (str[i] > 122 && str[i] < 127) || (str[i] == '\n') || (str[i] == '\t'))
+{
+if ((str[i + 1] > 96) && (str[i + 1] < 123))
+{
+str[i + 1] = str[i + 1] - 32;
 }
-			return (str);
-	
 }
-  
+++i;
+}
+return (str);
+}
