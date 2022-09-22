039b81e254795069039ef3b59033e58f8370020f
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-22 23:18:28 +0200

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,22 +11,22 @@ char *leet(char *str)
 	int i;
 	int j;
 char letter[5][3] = {
-	{'A','a','4'},
-	{'E','e','3'},
-	{'O','o','0'},
-	{'T','t','7'},
-	{'L','l','1'}
+	{'A', 'a', '4'},
+	{'E', 'e', '3'},
+	{'O', 'o', '0'},
+	{'T', 't', '7'},
+	{'L', 'l', '1'}
 };
 	i = 0;
-	while (str[i] != '\0')
+while (str[i] != '\0')
+{
+	for (j = 0; j < 5; j++)
 	{
-		for (j = 0; j < 5; j++)
-		{
 		if (str[i] == letter[j][0] || str[i] == letter[j][1])
 		{
 			str[i] = letter[j][2]
-			}
 		}
 	}
+}
 return (str);
 }
