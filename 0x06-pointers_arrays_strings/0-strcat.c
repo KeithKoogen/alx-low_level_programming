e2ea66ceb146d27c5b3bd0114c0ba09ded8dd2a2
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-22 15:38:09 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,7 +13,9 @@ int len_src;
 int len_dest;
 int i;
 len_src = sizeof(src) / sizeof(char);
-len_dest = sizeof(dest) / sizeof(char);
+len_dest = 0;
+while (dest[len_dest] != '\0')
+len_dest++;
 
 
 for (i = 0; i < len_src - 1; i++)
