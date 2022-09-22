2cd35134b068de457944e1036d7876f623b82308
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-22 15:46:54 +0200

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,6 +5,7 @@
 * @dest: a string
 * @src: a string
 * @n: an integer number of characters to copy
+* Return: A pointer
 */
 
 char *_strncat(char *dest, char *src, int n)
@@ -12,13 +13,14 @@ char *_strncat(char *dest, char *src, int n)
 int length;
 int i;
 length = 0;
-while (dest[length] != '\0') 
+while (dest[length] != '\0')
 {
 ++length;
 }
 
 for (i = 0; i < n; i++)
-dest[length + i] = src[i];
+dest[length++] = src[i];
 
+dest[length] = '\0';
 return (dest);
 }
