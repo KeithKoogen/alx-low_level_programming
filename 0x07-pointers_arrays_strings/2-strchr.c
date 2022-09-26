c7a59ce8cc482928156b4a0902242e1df249cbc3
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-26 10:08:33 +0200

    Create 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strchr - locates a character in a string
+* @s: a string
+* @c: a char to look for
+* Return: a pointer to the first occurance of the character;
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	i = 0;
+	while (s[i] != \0)
+	{
+		if (s[i] == c)
+		{
+			return (*s[i]);
+			break;
+		}
+	}
+	return (NULL);
+}
