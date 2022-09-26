d337be21e522bfdf729cc81e7ae98f944e124aef
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-26 09:54:30 +0200

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _memset -  fills memory with a constant byte
+* Return: a char
+* @s: a string
+* @b: a char to be in all bytes
+* @n: integer number of bytes
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	
+	return (s);
+	
+}
