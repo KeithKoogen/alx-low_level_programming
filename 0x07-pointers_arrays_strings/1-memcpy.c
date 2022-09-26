f92dba44df152c055abe8b6bcb719849a7726374
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-26 10:00:10 +0200

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memcpy - copies memory area
+* @dest: destination memory
+* @src: source memory
+* @n: an unsigned in
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
