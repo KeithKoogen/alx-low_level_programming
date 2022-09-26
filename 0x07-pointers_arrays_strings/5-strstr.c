2785831084a17ce1aebe995aa1145057ad453eb6
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-26 11:28:56 +0200

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* _strstr - locates a substrin
+* @haystack: a string to search in
+* @needle: a string to search for
+* Return: a pointer to the beginning of the located substring, or NULL if the substring is not found
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	
+	int i, j, k, m;
+	
+	i = 0;
+	j = 0;
+	
+	while (needle[i] != '\0')
+		++i;
+	
+	while (haystack[j] != '\0')
+	{
+		
+		if (haystack[j] == needle[0])
+		{
+			m = 1;
+			for (k = 1; k < i; k++)
+			{
+				if (haystack[j + k] == needle[k])
+				{
+					m++;
+				}
+						
+			}
+					
+		}
+		if (m == i)
+			{
+				return (haystack + j);
+			}
+		
+		++j;
+	}
+	
+}
