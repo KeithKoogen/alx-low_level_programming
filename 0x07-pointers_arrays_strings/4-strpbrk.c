e9837e639802bf57a2690489aec83c66cebc9c75
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-26 11:00:11 +0200

    Create 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _strpbrk - searches a string for any of a set of byte
+* @s: a string
+* @accept: a string to look for
+* Return: a string with only the matching characters
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, k, m;
+	
+	i = 0;
+	j = 0;
+	m = 0;
+	while (accept[i] != '\0')
+	{
+		++i;
+	}
+	
+	while (s[j] != '\0')
+	{
+		for (k = 0; k < i; k++)
+		{
+			if (s[j] == accept[k])
+					return (s[j]);
+		}
+	}
+	
+	return (NULL);
+}
