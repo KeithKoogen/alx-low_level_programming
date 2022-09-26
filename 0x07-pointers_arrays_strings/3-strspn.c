1a6277b0c74ff5b4a762f66c6768c864c9d83d75
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-26 10:35:01 +0200

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* _strspn - gets the length of a prefix substring
+* @s: a string
+* @accept: a substring
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count;
+	int i, j, k;
+	i = 0;
+	j = 0;
+	k = 0;
+	
+	while (accept[i] != '\0')
+		++i;
+	
+	while (s[j] != '\0')
+	{
+		for (k = 0; k < i; k++)
+		{
+			if (s[j] == accept[i])
+				count++;
+		}
+		++j;
+	}
+	
+	return (count);
+}
