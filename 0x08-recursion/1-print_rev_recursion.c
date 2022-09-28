a74c55d3be4d0ec32320f39b730fe0c698937cd6
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-28 11:40:29 +0200

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - prints a string in reverse
+* @s: a string
+* Return: void
+*/
+
+_print_rev_recursion(char *s)
+{
+	
+	if (*s == '\0')
+	{
+		_putchar(*s - 1);
+		return;
+	}
+	_print_rev_recursion(s + 1);
+}
