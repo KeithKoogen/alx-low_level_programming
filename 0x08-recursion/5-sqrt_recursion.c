02ff6820d9e0e5a9ddf28a1e3fc809b0b064d178
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-28 21:12:30 +0200

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _sqrt_recursion - returns the natural square root of a number
+* @n: a number
+* Return: an Integer
+*/
+
+int _sqrt_recursion(int n)
+{
+	int sqrt;
+	sqrt = n - 1;
+	
+	if (sqrt * sqrt == n)
+	{
+		return (sqrt);
+	}
+	else if (sqrt * sqrt != n && sqrt < n)
+	{
+		sqrt++;
+		_sqrt_recursion(n);
+	}
+	else
+	{
+		return (-1);
+	}
+	
+}
