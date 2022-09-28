15211d1708a3bfd51c209db4ac6679e5e7124104
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-28 19:44:59 +0200

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,27 +8,25 @@
 
 int _strlen_recursion(char *s)
 {
-	
+
 	int len;
-	
+
 	len = 0;
-	
+
 	if (*s != '\0')
 	{
-		
-		
-		
+
+
+
 		len++;
-		
+
 	}
 	else
 	{
 		return (len);
 	}
-	
-len += 	_strlen_recursion(s + 1);
-return (len);
-	
 
-	
+len += _strlen_recursion(s + 1);
+return (len);
+
 }
