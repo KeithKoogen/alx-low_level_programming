6bebf75ff012d14592b3aba5077e565a6bcd7b7a
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-28 11:47:18 +0200

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,13 @@
 
 void _print_rev_recursion(char *s)
 {
-	
-	if (*s == '\0')
+
+	if (*s != '\0')
 	{
-		_putchar(*s - 1);
-		return;
+		_print_rev_recursion(s + 1);
+
 	}
-	_print_rev_recursion(s + 1);
+	
+	_putchar(*s);
+	return;
 }
