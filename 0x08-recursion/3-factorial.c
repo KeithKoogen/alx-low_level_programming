d2a7e14ed01d968fa8fb86e84db3458c45fd7bf7
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-28 19:51:05 +0200

    Create 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* factorial - returns the factorial of a given number
+* @n: a given number
+* Return: a factorial in integer
+*/
+
+int factorial(int n)
+{
+	
+	int fact;
+	
+	fact = 0;
+	if (n = 1)
+	{
+		return(fact)
+	}
+	
+	fact += factorial(n - 1);
+	
+	return (fact);
+	
+}
