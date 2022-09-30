2c8878e199d759f39f01fed98ce441ef45360a90
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-09-30 08:04:51 +0200

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - print number of arguments
+* @argv: arguments
+* @argc: number of arguments
+* Return: an integer 0
+*/
+
+int main(int argc, char *argv[])
+{
+	
+	printf("%d\n", argc);
+	return (0);
+}
