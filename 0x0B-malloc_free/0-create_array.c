0608d9b70f0e0a5bfc57f27a4d4195a2fb0d00ce
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-03 08:08:35 +0200

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,23 +9,23 @@
 
 char *create_array(unsigned int size, char c)
 {
-	
+
 	char *p;
 	unsigned int i;
-	
+
 	p = malloc(sizeof(char) * size);
-	
+
 	if (p == NULL)
 	{
 		printf("failed to allocate memory");
 		return (NULL);
 	}
-	
+
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
 	}
-	
+
 	return (p);
-	
+
 }
