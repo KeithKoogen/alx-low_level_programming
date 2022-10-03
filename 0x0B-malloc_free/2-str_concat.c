cbed034672907cff13b218400151d48ac8c3f6c2
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-03 08:39:00 +0200

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,29 +9,29 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	
+
 	int i, j, k;
 	char *ptr;
-	
+
 	i = 0;
 	j = 0;
-	
+
 	if (s1 != NULL)
 	while (s1[i] != '\0')
 		++i;
-	
+
 	if (s2 != NULL)
 	while (s2[j] != '\0')
 		++j;
-	
+
 	ptr = malloc((sizeof(char) * (i + j)) + 1);
-	
+
 	if (ptr == NULL)
 		return (NULL);
-	
+
 	for (k = 0; k < i; k++)
 		ptr[k] = s1[k];
-	
+
 	for (k = 0; k < j; k++)
 		ptr[i + k] = s2[k];
 	ptr[i + k + 1] = '\0';
