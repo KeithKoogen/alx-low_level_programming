b173da89b4ce0399279c8bde9360e6a8f6847008
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-03 10:21:55 +0200

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	
-	ptr = malloc(sizeof(ptr) * (width - 2) * (height - 2));
+	ptr = malloc((sizeof(ptr) * height) + (sizeof(int) * width * height));
 		
 	
 	if (ptr == NULL)
