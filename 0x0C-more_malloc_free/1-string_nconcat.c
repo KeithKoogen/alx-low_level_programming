4ca2d858592204d4906278f087c398be848c416e
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-06 08:36:10 +0200

    Create 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* string_nconcat -  concatenates two strings
+* @s1: a string
+* @s2: a string
+* @n: number of bytes to copy from a2
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	char *ptr;
+	unsigned int i, j , k, len_s1, len_s2;
+
+	len_s1 = strlen(s1);
+	len_s2 = strlen(s2);
+
+	if (len_s2 - n < 0)
+	{
+		n = len_s2;
+	}
+
+	ptr = malloc(sizeof(char) * (len_s1 + n + 1));
+	
+	if (ptr == NULL)
+	return (1);
+
+	for (i = 0; i < len_s1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+
+	++i;
+	for (k = 0; k < n; k++)
+	{
+		ptr[i] = s2[k];
+		i++;
+	}
+
+	ptr[i + 1] = '\0';
+
+	return (ptr);
+
+}
