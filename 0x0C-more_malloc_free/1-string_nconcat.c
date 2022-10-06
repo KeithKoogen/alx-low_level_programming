fcf3eae78b35f1c5183e3d2a4588be9ac8d11cd9
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-06 13:29:48 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,27 +17,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	
 	
 
-	len_s1 = strlen(s1);
-	len_s2 = strlen(s2);
-
-
-
-	if (n > 0)
-	ptr = malloc(sizeof(s1) + (n * sizeof(char *)));
-	
-	if (ptr == NULL)
-	return (NULL);
-
+	len_s1 = 0;
+	len_s2 = 0;
 	
 	if (n == 0)
 		return (s1);
 	
-	if (n > len_s2)
+	while (s1[len_s1] != '\0')
+		++len_s1;
+	
+	while (s2[len_s2] != '\0')
+		++len_s2;
+	
+	if (n >= len_s2)
 	{
 	n = len_s2;
 	}
-	
 
+
+	if (n > 0)
+	ptr = malloc((sizeof(s1)) + (n * sizeof(char *)));
+	
+	if (ptr == NULL)
+		return (NULL);
+
+	
 	for (i = 0; i < len_s1; i++)
 	{
 		ptr[i] = s1[i];
