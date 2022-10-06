153e7de4f04474753ccfbec441d2e90afa5779d4
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-06 09:05:51 +0200

    Create 3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* array_range - creates an array of integers
+* @min: int to start at
+* @max: int to end at
+* Return: a pointer to array of integers
+*/
+
+int *array_range(int min, int max)
+{
+	
+	int i, k, j;
+	int *ptr;
+	
+	i = 0;
+	k = 0;
+	
+	if (min > max)
+		return (NULL);
+	
+	for (j = min; j < max; j++)
+	{
+		++i;
+	}
+	
+	ptr = malloc(sizeof(int) * i);
+	if (ptr == NULL)
+		return (NULL);
+	
+	for (k = 0; k < i; k++)
+	{
+		ptr[k] = k + min;
+	}
+	
+	return (ptr);
+}
