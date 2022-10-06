44d52b54ca84ef1c5a6930d0d544b0449a02f6c7
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-06 15:25:19 +0200

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,16 +17,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	
+
 	ptr = malloc(total_size);
-	
+
 	ptr2 = ptr;
-	
+
 	if (ptr == NULL)
 		return (NULL);
 
-	for(i = 0; i < total_size; i++)
+	for (i = 0; i < total_size; i++)
 		*ptr++ = 0;
-	
-	return ptr2;
+
+	return (ptr2);
 }
