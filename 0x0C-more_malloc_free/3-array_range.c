a6d7d5560cc0f45edf4785bd3fa1662c896549af
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-06 09:06:32 +0200

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,29 +9,29 @@
 
 int *array_range(int min, int max)
 {
-	
+
 	int i, k, j;
 	int *ptr;
-	
+
 	i = 0;
 	k = 0;
-	
+
 	if (min > max)
 		return (NULL);
-	
+
 	for (j = min; j < max; j++)
 	{
 		++i;
 	}
-	
+
 	ptr = malloc(sizeof(int) * i);
 	if (ptr == NULL)
 		return (NULL);
-	
+
 	for (k = 0; k < i; k++)
 	{
 		ptr[k] = k + min;
 	}
-	
+
 	return (ptr);
 }
