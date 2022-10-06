4fbe514f99ab18cfb4bc524f09bd89a84137bd11
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-06 09:21:57 +0200

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,10 +10,37 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-
-void *newptr;
-int i, j;
-
-if 
-
+	void *newptr;
+	int i, j;
+	
+	if (old_size == new_size)
+		return (ptr);
+	
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	
+	if (new_size > old_size && ptr != NULL)
+	{
+		newptr = malloc(new_size);
+		if (newptr == NULL)
+			return (NULL);
+		
+		for (i = 0; i < old_size; i++)
+		{
+			newptr[i] = ptr[i];
+		}
+		free(ptr);
+	}
+	
+	if (ptr == NULL)
+	{
+		newptr = malloc(new_size);
+		if (newptr == NULL)
+		return (NULL);
+	}
+	
+	return (newptr);
 }
