3ae7dc13b1db2d1aa12c383416bd8d8f8065da84
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-06 16:56:47 +0200

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+* numsonly - checks if a string contains only digits only
+* @str: a string
+* Return: a boolean
+*/
+
+bool numsonly(char *str);
+{
+	int i;
+	
+	i = 0;
+	
+	while (str[i] != '\0')
+	{
+	if (str[i] < 48 && str[i] > 57) 
+	return (false);
+	}
+	
+	return (true);
+}
+
+
+/**
 * main - multiplies two positive numbers
 * @num1: integer one
 * @num2: integer two
@@ -9,5 +31,31 @@
 
 int main(int argc, char *argv[])
 {
+	int *ptr;
+	int i, k;
+	
+		if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	
+	if (numsonly(argv[1]) != true || numsonly(argv[2]) != true)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	
+	i = atoi(argv[1]);
+	k = atoi(argv[2]);
+	
+	ptr = malloc(i * k);
+	
+	*ptr = i * k;
+	
+	printf("%d\n", *ptr);
+	
+	return (0);
+
 
 }
