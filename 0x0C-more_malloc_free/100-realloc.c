f2822a69d28e874ce67ae1b8a0e0996427ca8902
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-06 09:12:46 +0200

    Create 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _realloc - reallocates a memory block
+* @ptr: a pointer
+* @old_size: the old size
+* @new_size: the new size
+* Return: a pointer to a new memory block
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+void *newptr;
+int i, j;
+
+if 
+
+}
