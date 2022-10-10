63cd809ee3fcfc72333a7ade4b763334af02c4df
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-10 08:14:59 +0200

    Create 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+
+
+/**
+* init_dog - initialize a variable of type struct dog
+* @d: a pointer to a struct dog
+* @name: name of new variable
+* @age: age of dog
+* @owner: name oif owner
+* Retuen: void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+}
