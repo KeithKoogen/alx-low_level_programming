ad1ea3ee6d87ca7520e5253af45c87a32fc34f44
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-10 08:24:46 +0200

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-
+#include "dog.h"
 
 /**
 * init_dog - initialize a variable of type struct dog
@@ -11,5 +11,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;	
 
 }
