8636bf0f30dae73a42f909a24566452f95d25748
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-10 21:37:43 +0200

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,14 +5,16 @@
 * @d: a pointer to a struct dog
 * @name: name of new variable
 * @age: age of dog
-* @owner: name oif owner
+* @owner: name of owner
 * Retuen: void
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owner;	
-
+	if (d != NULL)
+	{
+		d -> name = name;
+		d -> age = age;
+		d -> owner = owner;
+	}
 }
