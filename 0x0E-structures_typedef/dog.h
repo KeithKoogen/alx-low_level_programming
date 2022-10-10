ceb0e67ff01d4ede40baf1ca6419d27452c784ee
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-10 21:34:44 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
 * struct dog - creates a dog
 * @name: name of dog
@@ -21,3 +24,7 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
+
+
+
+#endif
