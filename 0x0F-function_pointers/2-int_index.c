78fe8583431fe5b89f1d1bd6627b8a8f7eeb3a42
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-12 12:26:17 +0200

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+/**
+* int_index - searches for an integer
+* @array: an array of int
+* @size: size of array
+* @cmp: pointer to function
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	
+	int (*ptr)(int);
+	int i;
+
+	ptr = cmp;
+
+	if (size <= 0)
+		return (-1);
+
+	while (i < size)
+	{
+		if ((*ptr)(array[i]) != 0)
+			return (i);
+		++i;
+	}
+
+
+	return (-1);
+
+}
