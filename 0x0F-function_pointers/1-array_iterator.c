bcd8eb849abb5e7d6a253603f76a889c33e1981e
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-12 11:58:52 +0200

    Create 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+
+/**
+* array_iterator -  executes a function given as a parameter
+* @array: an array of integers
+* @size: size of array
+* @action: a function pointer
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	i = 0;
+
+	void (*ptr)(int);
+	ptr = action;
+
+	while (i < size)
+	{
+		(*ptr)(array[i]);
+		++i;
+	}
+
+}
