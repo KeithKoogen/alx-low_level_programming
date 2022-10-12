d50cc2cf2c828c11104cae25ad765bf9f20d916e
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-12 23:16:52 +0200

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,33 +15,34 @@ int main(int argc, char *argv[])
 	int a, b, i, (*n)(int, int);
 	char *s;
 
-	if (argc == 4)
-	{
-		a = atoi(argv[1]);
-		b = atoi(argv[3]);
-
-		s = argv[2];
-		if ((strcmp(s, "/") || strcmp(s, "%")) && (b == 0))
-		{
-			printf("Error\n");
-			exit(100);
-		}
-		n = get_op_func(s);
-
-		if (n == NULL)
-		{
-			printf("Error\n");
-			exit(99);
-		}
-
-		i = (*n)(a, b);
-
-		printf("%d\n", i);
-		return (0);
-	}
-	else
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	s = argv[2];
+	if ((strcmp(s, "/") || strcmp(s, "%")) && (b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	n = get_op_func(s);
+
+	if (n == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	i = (*n)(a, b);
+
+	printf("%d\n", i);
+	return (0);
+
+	
+	
 }
