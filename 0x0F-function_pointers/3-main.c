c773e9f081f763e8f67b4eb9cf746cb6ac8e0f4d
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-12 13:14:49 +0200

    Create 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+
+/**
+* main - performs different ops on numbers based on input
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: void
+*/
+
+int main(int argc, char *argv[])
+{
+	int a, b, n;
+	char *s;
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	s = argv[2];
+
+
+	n = (*get_op_func(s))(a, b);
+	printf("%d\n", n);
+
+	return (0);
+
+}
