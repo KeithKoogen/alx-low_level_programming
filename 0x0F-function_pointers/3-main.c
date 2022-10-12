681df9dd0d4e2f047140c3dd80b65ac764f773bb
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-12 13:25:45 +0200

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,14 +13,17 @@ int main(int argc, char *argv[])
 {
 	int a, b, n;
 	char *s;
+	
+	if (argc == 3)
+	{
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	s = argv[2];
 
-
 	n = (*get_op_func(s))(a, b);
 	printf("%d\n", n);
+	}
 
 	return (0);
 
