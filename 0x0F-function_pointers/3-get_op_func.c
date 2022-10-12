ec9eecc0d64ae31507646ce2b261c1b91a0d58e4
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-12 13:02:51 +0200

    Create 3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+* get_op_func - selects correct function to perform
+* @s: a string
+* Return: an integer
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+
+	i = 0;
+
+	while (i < 5)
+	{
+		if (ops[i][0] == s)
+			return (ops[i][1]);
+	}
+
+	return (NULL);
+}
