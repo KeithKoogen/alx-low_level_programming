22997b3210dcfbf491389471872eb65fdde61ef2
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-14 00:06:37 +0200

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -99,7 +99,7 @@ void print_all(const char * const format, ...)
 	{
 		functs = get_op_func(format[i]);
 		
-		functs(va_arg(args, return_type(format[i])));
+		functs(va_arg(args, format[i]));
 
 						 
 		++i;
