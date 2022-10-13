70616b99dc8e678aaf004b98461ad22476dc8200
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-13 22:51:06 +0200

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,55 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+typedef struct funs
+{
+	char s;
+	(*funct)();
+} funs;
 
+void print_int()
+{
+	printf("%d", va_arg(args, int));
+}
+
+void print_char()
+{
+	printf("%c", va_arg(args, int));
+}
+
+void print_float()
+{
+	printf("%f", va_arg(args, double));
+}
+
+void print_string(char *s)
+{
+	printf("%s", va_arg(args, char *));
+}
+
+(*func)()function_type(char c)
+{
+	funs fun[] = {
+		{'i', print_int},
+		{'c', print_char},
+		{'f', print_float},
+		{'s', print_string}
+	};
+	int i;
+	
+	i = 0;
+	
+	while (i < 4)
+	{
+		if (fun[i].s == c)
+		{
+			return (fun[i].funct);
+		}
+		++i;
+	}
+	
+	return (NULL);
+}
 
 /**
 * print_all - prints anything
@@ -12,38 +60,14 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i;
+	(*functs)();
 	
 	va_start(args, format);
 	i = 0;
 	while (format[i] != '\0')
 	{
-		if (format[i] == 'i')
-		{
-			printf("%d", va_arg(args, int));
-			if (format[i + 1] != '\0')
-				printf(", ");
-		}
-		
-		if (format[i] == 'c')
-		{
-			printf("%c", va_arg(args, int));
-			if (format[i + 1] != '\0')
-				printf(", ");
-		}
-		
-		if (format[i] == 'f')
-		{
-			printf("%f", va_arg(args, double));
-			if (format[i + 1] != '\0')
-				printf(", ");
-		}
-		
-		if (format[i] == 's')
-		{
-			printf("%s", va_arg(args, char *));
-			if (format[i + 1] != '\0')
-				printf(", ");
-		}
+		*functs = function_type(format[i]);
+		functs();
 
 						 
 		++i;
