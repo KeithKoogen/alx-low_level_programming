5f9961e6b41bb03b227f4d3f6eb7935addb0534b
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-13 11:55:01 +0200

    Create 2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_strings - prints strings, followed by a new line
+* @separator: string to be printed between the strings
+* @n: number of strings passed to the function
+* Return: Void
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+}
