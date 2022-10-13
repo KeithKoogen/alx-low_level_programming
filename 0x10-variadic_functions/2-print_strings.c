7ac477372eb0a021eef4c1a0dee99ca166206128
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-13 12:02:45 +0200

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,5 +10,26 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+  unsigned int i;
+  va_list args;
 
+  va_start(args, n);
+
+  for (i = 0; i < n; i++)
+  {
+		if (va_arg(args, char *) != NULL)
+		{
+			printf("%s", va_arg(args, char *));
+		}
+		else
+		{
+			printf("(nil)");
+		}
+
+		if (i < (n - 1) && seperator != NULL)
+			printf("%s", seperator);
+  }
+
+  va_end(args);
+	printf("\n");
 }
