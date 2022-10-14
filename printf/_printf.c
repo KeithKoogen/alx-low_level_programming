439b17f5a5fc8c597fc343f1a3a1fef305f45cef
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-14 12:38:38 +0200

    Update _printf.c

--- printf/_printf.c
+++ printf/_printf.c
@@ -2,7 +2,22 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+print_char(va_list *ptr)
+{
+  int i;
+  i = va_arg(*ptr, char);
+ _putchar(i);
+}
 
+print_string(va_list *ptr)
+{
+  char *str;
+  int i;
+  
+  str = va_arg(*ptr, char *);
+  while (str[i] != '\0')
+  	_putchar(str[i]);
+}
 
 int _printf(const char *format, ...)
 {
