4784df861a2705154852848e9d962f18b7bb06b0
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-14 12:28:59 +0200

    Update _printf.c

--- printf/_printf.c
+++ printf/_printf.c
@@ -1,7 +1,18 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+
 
 int _printf(const char *format, ...)
 {
+  function functions[] = {
+    {'c', print_char},
+    {'s', print_string}
+  };
+  
+  va_list args, *ptr;
+  
   
 
 }
