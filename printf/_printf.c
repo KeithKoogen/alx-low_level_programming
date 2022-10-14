6f297a80d966f74e6989172baecc5eaaf77c41e8
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-14 12:50:01 +0200

    Update _printf.c

--- printf/_printf.c
+++ printf/_printf.c
@@ -15,6 +15,7 @@ print_string(va_list *ptr)
   int i;
   
   str = va_arg(*ptr, char *);
+	
   while (str[i] != '\0')
   	_putchar(str[i]);
 }
@@ -25,9 +26,25 @@ int _printf(const char *format, ...)
     {'c', print_char},
     {'s', print_string}
   };
+	int i;
   
   va_list args, *ptr;
   
+	va_start(args, format);
+	while (format != '\0')
+	{
+		i = 0;
+		while (i < 2)
+		{
+			if (*format == '%')
+				if (functions[i].symbol == *(format + 1))
+					functions.function(ptr);
+			++i;
+		}
+		++format;
+	}
+		
+	va_end(args);
   
 
 }
