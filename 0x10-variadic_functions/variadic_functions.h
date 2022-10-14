ee460fd6fe6ea860237f92c22964925606c25012
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-14 10:15:02 +0200

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,12 @@
 #ifndef VAR_FUNCS
 #define VAR_FUNCS
 
+typedef struct funs
+{
+	char s;
+	void (*funct)();
+}fun;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
