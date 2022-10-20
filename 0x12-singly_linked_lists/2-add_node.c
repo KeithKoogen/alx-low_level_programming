c7c81f4009eb1b00aba480d803c4d69bc2515c4c
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-20 23:11:45 +0200

    Create 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+* add_node - adds a new node
+* @head: pointer to head of linked list
+* @str: a string
+* Return: a pointer to thenew node
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	
+	new = malloc(sizeof(list_t));
+	
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	
+	new->next = head;
+	head = new;
+	new->str = strdup(str);
+	new->len = strlen(str);
+	
+	return (head);
+	
+}
