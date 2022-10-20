14896cdd9ad4201089d0e0cb9bc6f20a9c1d0831
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-20 23:28:53 +0200

    Create 3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+* add_node_end - adds a node at end of linked list
+* @head: head of linke list
+* @str: a string
+* Return: address of new element or NULL
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	
+	new = malloc(sizeof(list_t));
+	
+	if (new == NULL)
+		return (NULL);
+	
+	while (*head !=NULL)
+	{
+		if (*head->next == NULL)
+		{
+			*head->next = new;
+			new->next = NULL;
+			new->str = strdup(str);
+			new->len = strlen(str);
+			return (new);
+			
+		}
+		*head = *head->next;
+	}
+}
