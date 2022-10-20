a4527b8aab9ba5b81f8d2b61963d130451d75b7a
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-21 00:09:40 +0200

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -24,7 +24,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->str = strdup(str);
 	new->len = strlen(str);
 	
-	if (*head = NULL)
+	if (*head == NULL)
 	{
 		*head = new;
 		return (*head);
