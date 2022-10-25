21dd95f5b6cc97507cc3ea2791e56c2d6e930440
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-25 12:13:09 +0200

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,6 +11,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	if (index == 0)
 		return (head);
+	if (head == NULL)
+		return (NULL);
 
 	return (get_nodeint_at_index(head->next, index - 1));
 }
