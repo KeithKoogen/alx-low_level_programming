a9512c246b79bca4268e0e44a55bf52d0e1eda3b
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-25 12:00:08 +0200

    Create 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - returns the nth node of a listint_t linked list
+* @head: the head of the linked list
+* @index: the index number of the node requested
+* Return: a pointer to the node at index
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	if (index == 0 && head != NULL)
+		return (head);
+
+	if (head == NULL)
+		return (NULL);
+
+	get_nodeint_at_index(head->next, index - 1);
+
+}
