144834a0f3996ace8d397b3f61d4ff3e6ad0bb6e
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-25 10:42:11 +0200

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,7 +7,6 @@
 * Return: a pointer to the linked list
 */
 
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *ptr;
@@ -27,7 +26,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new_node;
 		return (new_node);
 	}
-	
 
 	while (ptr->next != NULL)
 		ptr = ptr->next;
