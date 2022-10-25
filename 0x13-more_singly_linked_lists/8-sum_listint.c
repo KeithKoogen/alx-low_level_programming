fac21768dae5de67ee91ff1952cf87c03b98922e
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-25 12:21:24 +0200

    Create 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+* sum_listint - adds all integers in list
+* @head: the head of the list
+* Return: the sum off all data(n)
+*/
+
+int sum_listint(listint_t *head)
+{
+	if (head->next == NULL)
+		return (head->n);
+	
+	return (head->n + sum_listint(head->next);
+}
