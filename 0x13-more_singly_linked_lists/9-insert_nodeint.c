bfcc4eaf7685778d849a9d8945e0a7e84b3fd6b2
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-25 13:21:36 +0200

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,8 +29,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 	}
 	
-	tmp = ptr;
-	ptr = newnode;
+	tmp = ptr->next;
+	ptr->next = newnode;
 	newnode->next = tmp;
 
 	return (newnode);
