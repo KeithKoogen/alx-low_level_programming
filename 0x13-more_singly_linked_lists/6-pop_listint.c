58ab58b7d8dbd3008627c9c3b01e52ff9597be90
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-25 11:49:15 +0200

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
-* 
+* pop_listint - deletes the head node of a linked list
+* @head: the head of the list
+* Return: the values stored in the head
+*/
 
 int pop_listint(listint_t **head)
+{
+int i;
+listint_t *ptr, *tmp;
+	
+ptr = *head;
+i = 0;
+i = ptr->n;
+tmp = ptr->next;
+
+free(*head);
+*head = tmp;
+
+return (i);
+}
