80ac1bb716b1e43095af7a9a5572d8f930b8152b
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-25 10:28:32 +0200

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,7 +11,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 
-	listint_t *new_node, *ptr;
+	listint_t *new_node;
 
 	new_node = malloc(sizeof(listint_t));
 
@@ -21,12 +21,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new_node->next = NULL;
 	new_node->n = n;
 
-	ptr = *head;
 
-	while (ptr->next != NULL)
-		ptr = ptr->next;
 
-	ptr->next = new_node;
+	while (*head->next != NULL)
+		*head = *head->next;
+
+	*head->next = new_node;
 
 	return (new_node);
 
