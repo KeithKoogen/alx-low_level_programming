e6c54d29991815b009052fcadb1da4b5e7db5306
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-25 13:24:00 +0200

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,15 +12,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr, *newnode, *tmp;
 	unsigned int i;
-	
+
 	ptr = *head;
+
+	if (ptr == NULL)
+	return (NULL);
+
 	newnode = malloc(sizeof(listint_t));
-	
+
 	if (newnode == NULL)
 		return (NULL);
-	
+
 	newnode->n = n;
-	
+
 	for (i = 0; i < idx - 1; i++)
 	{
 		ptr = ptr->next;
@@ -28,11 +32,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (ptr == NULL)
 			return (NULL);
 	}
-	
+
 	tmp = ptr->next;
 	ptr->next = newnode;
 	newnode->next = tmp;
 
 	return (newnode);
-	
+
 }
