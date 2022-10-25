0587026fe3f2e8b9aa770cb5991b73cfd707858b
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-25 13:09:09 +0200

    Create 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+* insert_nodeint_at_index - inserts a note at a given index
+* @head: the head of the node
+* @idx: the index
+* @n: an integer value of the new node
+* Return: a pointer to a listint_t
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *ptr, *newnode, *tmp;
+	unsigned int i;
+	
+	ptr = *head;
+	newnode = malloc(sizeof(listint_t));
+	
+	if (newnode == NULL)
+		return (NULL);
+	
+	newnode->n = n;
+	
+	for (i = 0; i < idx; i++)
+	{
+		ptr = ptr->next;
+	}
+	
+	tmp = ptr->next;
+	ptr->next = newnode;
+	newnode->next = tmp;
+
+	return (newnode);
+	
+}
