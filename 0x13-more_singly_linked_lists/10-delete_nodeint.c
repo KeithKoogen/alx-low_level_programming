3bf5b05c79470459fe79c44a112c89e58f65f9b1
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-25 14:52:57 +0200

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,29 +15,27 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *ptr, *tmp2;
 
 	ptr = *head;
-	
+
 	if (ptr == NULL)
 		return (-1);
 
 	i = 0;
-	
+
 	if (index == 0)
 	{
 		tmp = ptr->next;
 		free(ptr);
 		*head = tmp;
-		
 	}
 
 	while (i < index)
 	{
 		ptr = ptr->next;
 		++i;
-		
+
 		if (ptr == NULL)
 			return (-1);
 	}
-	
 
 	tmp = ptr->next;
 	tmp2 = tmp->next;
