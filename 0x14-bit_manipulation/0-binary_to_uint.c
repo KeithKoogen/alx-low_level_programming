b2f1e4f762f64cebc5c7fe922f06c17caabe5f9a
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-27 10:03:13 +0200

    Create 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* binary_to_uint - converts a binary number to an unsigned int
+* @b: array pointing to a string of 0 and 1 chars
+* Return: an unsiged int value converted from binary to decimal
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i;
+	
+	if (b == NULL)
+		return (0);
+	
+	
+	
+	
+}
