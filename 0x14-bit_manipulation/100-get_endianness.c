1f64e4463587b6a54e268e72cd5bd6b78fe53a61
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-27 21:29:42 +0200

    Create 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* get_endianness - determines the endianess of a system
+* Return: 0 if BIg Endian 1 if Little Endian
+*/
+
+int get_endianness(void)
+{
+	unsigned int i;
+	
+	char *c = &i;
+	
+	i = 1;
+	
+	
+   if (*c)   
+       return (1);
+   else
+       return (0);
+	
+}
