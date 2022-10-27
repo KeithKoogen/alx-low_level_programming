395680681c76beb4022036d24c9aaaeb249befdf
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-27 20:55:59 +0200

    Create 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* flip_bits - counts the number of bits to flip from one number to another
+* @n: the number
+* @n: the number to flip
+* Return: Number of bits as an int
+*/
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int count;
+	unsigned int index;
+	
+	count = 0;
+	index = sizeof(n) * 8;
+	
+	return (index);
+}
