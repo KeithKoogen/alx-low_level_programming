797c1c91dea4b5de90b8028bf60eaedd02e39b88
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-27 20:29:26 +0200

    Create 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* clear_bit - sets a given index bit to 0
+* @n: a number
+* @index: the index of the bit
+* Return: 1 if works or -1 if error
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	*n = *n | 0 << index;
+
+	return (1);
+
+}
