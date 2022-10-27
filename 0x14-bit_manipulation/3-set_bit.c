138cb9744609973653a2473bb879d1f2e2b88477
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-27 20:17:20 +0200

    Create 3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* set_bit - sets a bit to 1
+* @n: the number
+* @index: the bit to change
+* Return: 1 if success or -1 if error
+*/
+
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+	
+	i = *n;
+	i |= 1 << k;
+	
+	return (i);
+	
+}
