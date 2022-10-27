d572d318586fd84b71461168e4b4d000edf6c668
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-27 10:14:55 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,12 +8,26 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i;
-	
+	unsigned int i, j, digit;
+
 	if (b == NULL)
 		return (0);
-	
-	
-	
-	
+
+	i = 0;
+	digit = 0;
+	j = 1;
+
+	while (b[i] != '\0')
+	{
+		if (b[i] < 48 || b[i] > 49)
+			return (0);
+
+		digit = (b[i] - 48) * j;
+
+		j = j * 2;
+		++i;
+	}
+
+	return (digit);
+
 }
