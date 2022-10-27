e895d1c765d0ce456ff3dd44ea092dc06f331a5b
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-27 17:04:29 +0200

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,6 +8,29 @@
 
 void print_binary(unsigned long int n)
 {
+	if (n == 0)
+		_putchar(48);
+	if (n == 1)
+		_putchar(49);
+	if (n > 1)
+	{
+		while (n != 0)
+		{
+			if (n & 1)
+			{
+				putchar(49);
+			}
+			else
+			{
+				putchar(48);
+			}
+			n >> 1;
+			
+		}
+	}
+	
+	_putchar('\n');
+
 	
 	
 }
