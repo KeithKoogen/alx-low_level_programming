cfdee92a102dd8e130aa9ed9bce7aebd85bea20a
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-27 18:55:52 +0200

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* get_bit - returns the values of a bit at given index
+* @n: the number
+* @index: the index
+* Return: an integer value of the bit;
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i;
+
+	if (index == 0)
+	{
+		i = n & 1;
+		return (i);
+	}
+
+	get_bit(n >> 1, --index);
+}
