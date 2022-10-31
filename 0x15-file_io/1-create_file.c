ba1909c6e10e5057d155040cca0ba04012b2b3d9
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-31 18:33:40 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		if (fd < 0)
 		{
 			close(fd);
-			fd = open(filename, O_CREAT | O_WRONLY | TRUNC | O_APPEND);
+			fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND);
 		}
 	else
 	{
