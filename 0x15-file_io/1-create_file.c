eb47a09eb6023a453a8e03122dc2bff166f67459
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-31 18:56:39 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,9 +9,10 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, check;
+	int fd, check, i;
 	
 	check = 0;
+	i = 0;
 	
 	if (filename == NULL)
 		return (-1);
@@ -24,9 +25,14 @@ int create_file(const char *filename, char *text_content)
 	
 	if (text_content != NULL)
 	{
-		check = write(fd, text_content, sizeof(text_content) - 2);
+		while (text_content[i] != '\0')
+		{
+		check = write(fd, &text_content[i], 1);
 		if (check == -1)
 			return (-1);
+		
+		++i;
+		}
 	}
 	
 	close(fd);
