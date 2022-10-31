a32f1a5f46a24911ba8a8ebaa75b189295bc7a45
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-31 16:52:34 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /*
-read_textfile - Reads and prints a text file
-@filename: name of the file
-@letters: number of letters to read an print
-Return: Actual number of letters it could read and print
+* read_textfile - Reads and prints a text file
+* @filename: name of the file
+* @letters: number of letters to read an print
+* Return: Actual number of letters it could read and print
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -32,21 +32,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		valid = read(fd, buf, sizeof(char));
 		if (valid == 0)
 			break;
-		
+
 		if (valid == -1)
 			break;
 
 		check = write(1, &buf[0], 1);
-		
+
 		if (check == -1)
 			return (0);
-		
+
 		if (buf[0] == '\0')
 			break;
 
 		if ((buf[0] > 31 && buf[0] < 127) || buf[0] == '\n')
 			++count;
-		
+
 	}
 
 
