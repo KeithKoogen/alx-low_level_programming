5cbed3d371744d6eef46ad09dcef590177327894
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-31 19:19:12 +0200

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, i;
 
 	i = 0;
-	
+
 	if (filename == NULL)
 		return (-1);
 
@@ -26,7 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		while (text_content[i] != '\0')
 		{
-			write(fd, &text_content[i], 1);			
+			write(fd, &text_content[i], 1);
 		}
 
 	}
