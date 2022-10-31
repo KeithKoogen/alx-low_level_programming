0b42ab9dd17c119a1a6f69241110f99400b2f43b
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-31 15:16:07 +0200

    Create 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/*
+read_textfile - Reads and prints a text file
+@filename: name of the file
+@letters: number of letters to read an print
+Return: Actual number of letters it could read and print
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, count;
+	char buf[1];
+	
+	buf[0] = 0;
+	count = 0;
+	
+	if (filename == NULL)
+		return(0);
+	
+	fd = open(filename, O_RDONLY);
+	
+	if (fd== -1)
+		return (0);
+	
+	while (buf != '\0')
+	{
+		read(fd, buf, 1);
+		_putchar(buf);
+		++count;
+		
+		if (count == letters)
+			break;
+
+	}
+	
+	return (count);
+}
