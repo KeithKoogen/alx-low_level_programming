89718d51189a0c9ddf049e4bf3a6019c20a5461e
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-31 19:51:59 +0200

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,13 +12,19 @@ int main(int argc, char *argv[])
 	int file_from, file_to, check;
 	char buffer[1024];
 	
+	if (argc != 3)
+	{
+		printf("Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	
 	file_from = open(argv[1], O_RDONLY);
 	
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC);
 	
 	while (check != 0)
 	{
-		check = read(file_from, buffer);
+		check = read(file_from, buffer, sizeof(buffer));
 		dprintf(file_to, buffer);
 	}
 	
