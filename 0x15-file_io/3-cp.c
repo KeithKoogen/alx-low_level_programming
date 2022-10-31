a401400cb879ed59c63c57c6fa20de2c0f14cbd1
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-31 20:32:57 +0200

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,23 +11,23 @@ int main(int argc, char *argv[])
 {
 	int file_from, file_to, check;
 	char buffer[1024];
-	
+
 	if (argc != 3)
 	{
 		printf("Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	
+
 	file_from = open(argv[1], O_RDONLY);
-	
+
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
-	
+
 	if (file_from < 0)
 	{
 		printf("Error: Can't read from file NAME_OF_THE_FILE\n");
 		exit(98);
 	}
-	
+
 	check = read(file_from, buffer, sizeof(buffer));
 	if (check < 0)
 	{
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	dprintf(file_to,"%s", buffer);
-	
+
 	close(file_to);
 	close(file_from);
 	return (0);
