93a2c62a85222e80e3b9a72819d53a399b7d7ad5
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-31 15:42:15 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t valid;
 	char *buf;
 	
-	buff = malloc(sizeof(char) * letters);
+	buf = malloc(sizeof(char) * letters);
 	
 	if (buff == NULL)
 		return (0);
