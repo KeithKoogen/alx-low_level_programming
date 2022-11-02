b340ee5994fbed01fd16b62725ed04e199918876
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-11-02 11:28:04 +0200

    Update 1_readlin.c

--- test/1_readlin.c
+++ test/1_readlin.c
@@ -42,14 +42,15 @@ int main(void)
   {  
   printf("$ ");  
   characters = getline(&buffer, &size, stdin);
-  }
+  } while (characters == 1);
 	
-		finalstring = malloc(sizeof(char *) * size);
-  while (characters == 1);
+	finalstring = malloc(sizeof(char *) * size);
+	
 	
 
 	
 	break_string(buffer, " ", finalstring);
+	printf("after break string function");
 	
 	while (finalstring[i] != NULL)
 	{
