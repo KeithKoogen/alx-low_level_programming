da2f641b1a6c958b11579f316e88dc23b3b6684d
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-11-02 16:39:48 +0200

    Create execute.c

---
+++ test/execute.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd>
+#include <stdlib.h>
+
+int main(void)
+{
+  char *argv[] = {"./add_numbers", NULL};
+  
+  printf("welcome to main program");
+  
+  execve(argv[0], argv, NULL);
+  
+  return (0);
+}
