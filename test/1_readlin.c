a43ec5bb1115108187522aba888d2889b35acf1c
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-11-02 10:36:07 +0200

    Create 1_readlin.c

---
+++ test/1_readlin.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+  char *buffer;
+  size_t size, characters;
+  
+  size = 32;
+  
+  
+  buffer = malloc(sizeof(char) * size));
+  
+  printf("$ ");
+  
+  characters = getline(&buffer, &size, stdin);
+  
+  printf("%s\n", buffer);
+  
+  return (0);
+  
+  
+}
