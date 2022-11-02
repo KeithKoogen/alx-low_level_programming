ed5c7c4fded001c4b072cefff5fab2a44f6c9ca5
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-11-02 11:08:16 +0200

    Update 1_readlin.c

--- test/1_readlin.c
+++ test/1_readlin.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+break_string(char *str, char *delimeter, char **ptr)
+{
+	char *token;
+	int i;
+	
+	i = 0;
+	
+	token = strtok(str, delimeter);
+	
+	while (token != NULL)
+	{
+		ptr[i] = token;
+		
+		token = (str, delimeter);
+		++i;
+	}
+	
+}
+
 
 int main(void)
 {
   char *buffer;
   size_t size, characters;
+	char **finalstring;
+	int i;
+	
+	i = 0;
   
   size = 32;
   
-  
   buffer = malloc(sizeof(char) * size);
+	
   
   do 
   {  
@@ -17,8 +42,17 @@ int main(void)
   characters = getline(&buffer, &size, stdin);
   }
   while (characters == 1);
+	
+	finalstring = malloc(sizeof(char) * size);
+	
+	breakstring(buffer, " ", finalstring);
+	
+	while (finalstring[i] != NULL)
+	{
   
-  printf("%s\n", buffer);
+  printf("%s\n", finalstring[i]);
+		++i;
+	}
   
   return (0);
   
