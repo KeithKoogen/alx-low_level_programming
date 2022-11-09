1163a5d0a939c370b82712557f9cfcc841c3154a
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-11-09 12:58:26 +0200

    Update 1_readlin.c

--- test/1_readlin.c
+++ test/1_readlin.c
@@ -41,7 +41,7 @@ void break_string(char *str, char *delimeter, char **ptr)
 	
 	while (token != NULL)
 	{
-		ptr[i] = malloc(sizeof(char) * strlen(token + 1));
+		ptr[i] = malloc(sizeof(char) * strlen(token));
 		strcpy(ptr[i], token);
 		token = strtok(NULL, " ");
 		++i;
