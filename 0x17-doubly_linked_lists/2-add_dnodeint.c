de55f941446cbb7af8f5dc63d48d13060121862f
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-08 13:02:59 +0200

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,8 +12,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	dlistint_t *new_node, *old_node;
 
-	if (n == NULL)
-		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
 	printf("here");
