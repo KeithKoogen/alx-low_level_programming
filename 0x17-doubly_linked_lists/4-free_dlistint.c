d7e3ea1a177977597614cd1dc030e22647ea8c43
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-08 13:31:23 +0200

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,10 +8,10 @@
 
 void free_dlistint(dlistint_t *head)
 {
-  if (head == NULL)
-  {
-      return;
-  }
-  free_dlistint(head->next);
-  free(head);
+	if (head == NULL)
+	{
+		return;
+	}
+	free_dlistint(head->next);
+	free(head);
 }
