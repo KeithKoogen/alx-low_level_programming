a465f8afe5240f53b9ade1c7eb7e8c225ee1cc6a
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-08 13:42:19 +0200

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,16 +10,16 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
-	
+
 	i = 0;
 	
 	if (head == NULL)
 		return NULL;
-	
+
 	while (head != NULL)
 	{
 		if (i == index)
-			return head
+			return (head);
 		++i;
 		head = head->next;
 	}
