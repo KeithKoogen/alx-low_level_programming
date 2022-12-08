cba6cd080b485392755a425f651bab8d992fb9c0
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-08 14:04:42 +0200

    Create 7-insert_dnodeint.c

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+* insert_dnodeint_at_index - inserts new node at a given position
+* @h: a pointer to the head of a linked list
+* @idx: the index required
+* @n: data to be added to new node a index
+* Return: a pointer to new node or NULL if failed
+*/
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *old_node;
+	unsigned int i;
+	
+	i = 0;
+	old_node = *h;
+
+	while (old_node != NULL)
+	{
+		if (i == idx)
+		{
+			break
+		}
+		++i;
+		old_node = old_node->next;
+	}
+	
+	if (i == idx)
+	{
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->prev = old_node->prev;
+		new_node->next = old_node;
+		old_node = new_node;
+		return (new_node);
+	}
+	else
+	{
+		return (NULL);
+	}
+	
+}
