cdfb38d92819de2784506f3555b7ebe26cc37519
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-08 15:02:01 +0200

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	old_node = *head;
 	if (old_node == NULL)
-		return (-1)
+		return (-1);
 	while (old_node != NULL)
 	{
 		if (index == i)
@@ -28,6 +28,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		old_node = tmp;
 		old_node->prev = old_node->next;
+		old_node->next->prev = old_node->prev;
 		free(tmp);
 		return (1);
 
