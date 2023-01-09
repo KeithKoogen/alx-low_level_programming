65495d7492bf986e9a7601f45bbae48824cf7a14
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2023-01-09 12:30:50 +0200

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,8 +9,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table;
+	unsigned long int i;
+	
+	i = 0;
+	
+	for (i = 0; i < size; i++)
+	{
+		new_hash_table[i] =  malloc(sizeof(hash_table_t));
+		
+	}
 
-	new_hash_table =  malloc(sizeof(hash_table_t) * size);
+	
 
 	if (new_hash_table == NULL)
 		return (NULL);
